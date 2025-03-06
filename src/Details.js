d0f6b8cb64399819fb5b3cb088c43774f3452803
Sree-harshitha <118330865+Sree-harshitha@users.noreply.github.com>
2025-03-06 19:46:42 +0530

    Added new files

---
+++ src/Details.js
@@ -0,0 +1,175 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+// import projectImage4 from "./assets/projects/project4.jpg";
+// import projectImage5 from "./assets/projects/project5.jpg";
+// import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Code Ninja",
+  tagline: "I build things for web",
+  img: profile,
+  about: `I am an ambitious young programmer who wants to keep learning and improving my knowledge. I have a great interest in new technologies and keeping up with new developments in the industry. I'm a great problem solver problems and have a knack for finding creative solutions to difficult problems.I am always looking for new challenges and am enthusiastic about learning new things.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/sri-ram-prasad-adusumilli/",
+  github: "https://www.github.com/CodeNinja-194",
+  // twitter: "https://twitter.com/",
+  // instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Frontend Web Developer",
+    Company: `APSSDC`,
+    Location: "Tanuku",
+    Type: "Full Time",
+    Duration: "July 2020 - Jan 2021",
+  },
+  {
+    Position: "Machine Learning Intern",
+    Company: `Indian Servers`,
+    Location: "Vijayawada",
+    Type: "Internship",
+    Duration: "July 2021 - July 2022",
+  },
+  {
+    Position: "Cyber Security Intern",
+    Company: `Spypro Security Solutions`,
+    Location: "Vijayawada",
+    Type: "Internship",
+    Duration: "May 2023 - July 2023",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Diploma in Computer Science Engineering",
+    Company: "A.A.N.M & V.V.R.S.R POLYTECHNIC ",
+    Location: "Gudlavalleru",
+    Type: "Full Time",
+    Duration: "Jan 2018 - Sept 2021",
+  },
+  {
+    Position: "Bachelor in Computer Science & Engineering",
+    Company: `R.V.R & J.C COLLEGE OF ENGINEERING`,
+    Location: "Guntur",
+    Type: "Full Time",
+    Duration: "Dec 2021 - Present",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Project title 1",
+    image: projectImage1,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 2",
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 3",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+//   {
+//     title: "Project title 4",
+//     image: projectImage4,
+//     description: `This is sample project description random things are here in description This is sample
+// project lorem ipsum generator for dummy content`,
+//     techstack: "HTML/CSS, JavaScript",
+//     previewLink: "https://google.com",
+//     githubLink: "https://github.com",
+//   },
+//   {
+//     title: "Project title 5",
+//     image: projectImage5,
+//     description: `This is sample project description random things are here in description This is sample
+// project lorem ipsum generator for dummy content`,
+//     techstack: "HTML/CSS, JavaScript",
+//     previewLink: "https://google.com",
+//     githubLink: "https://github.com",
+//   },
+//   {
+//     title: "Project title 6",
+//     image: projectImage6,
+//     description: `This is sample project description random things are here in description This is sample
+// project lorem ipsum generator for dummy content`,
+//     techstack: "HTML/CSS, JavaScript",
+//     previewLink: "https://google.com",
+//     githubLink: "https://github.com",
+//   },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "sriramadusumilli98@gmail.com",
+  phone: "+91 9849145615",
+};
